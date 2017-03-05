bfc677ea0f6df736a4b8b0a8689f537585cadf4c
jjatem <jjatem@hotmail.com>
2017-03-05 15:54:58 -0500

    Adding Initial Solution with Method to Compress/Zip Base64 encoded in memory

---
+++ ZipFileUtilsConsole/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZipFileUtilsLib;
+using System.IO;
+
+namespace ZipFileUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FileReader = new StreamReader(@"C:\Temp\Zip\AppSettings.json");
+
+            var bytesArray = default(byte[]);
+            using (var memstream = new MemoryStream())
+            {
+                FileReader.BaseStream.CopyTo(memstream);
+                bytesArray = memstream.ToArray();
+            }
+
+
+            string Base64EncodedFile = Convert.ToBase64String(bytesArray);
+
+            ZippedMemoryFile ZippedFileData = ZipFileUtilsLib.ZipFileUtils.GetZippedMemoryFile("AppSettings.json", Base64EncodedFile);
+
+            if (ZippedFileData != null && ZippedFileData.ZippedFileInMemory != null)
+            {
+                FileStream file = new FileStream(@"C:\Temp\Zip\" + ZippedFileData.Filename, FileMode.Create, FileAccess.Write);
+                ZippedFileData.ZippedFileInMemory.WriteTo(file);
+                file.Close();
+            }
+
+        }
+    }
+}
---
+++ ZipFileUtilsLib/ZipFileUtils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipFileUtilsLib
+{
+    public class ZippedMemoryFile
+    {
+        public string Filename { get; set; }
+        public MemoryStream ZippedFileInMemory { get; set; }
+    }
+
+    public class ZipFileUtils
+    {
+        public static ZippedMemoryFile GetZippedMemoryFile(string FileName, string Base64EncodedContents)
+        {
+            ZippedMemoryFile rv = new ZippedMemoryFile();
+
+            try
+            {
+                /*
+                 * Convert Base64 File Contents to Byte Array
+                 */
+                byte[] Base64DecodedFile = Convert.FromBase64String(Base64EncodedContents);
+
+                /*
+                 * Compress/Zip file and return it in a MemoryStream
+                 */
+                var compressedFileOutStream = new MemoryStream();
+
+                using (var MyCompressor = new ZipArchive(compressedFileOutStream, ZipArchiveMode.Create, true))
+                {
+                    var compressedFile = MyCompressor.CreateEntry(FileName, CompressionLevel.Optimal);
+                    using (var entryStream = compressedFile.Open())
+                    {
+                        using (var FileToCompressStream = new MemoryStream(Base64DecodedFile))
+                        {
+                            FileToCompressStream.CopyTo(entryStream);
+                        }
+
+                        rv.Filename = FileName + ".zip";
+                        rv.ZippedFileInMemory = compressedFileOutStream;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"An error has ocurred Compressing Base64 encoded file with filename {FileName}. Error Message was: {ex.Message} ");
+            }
+
+            return rv;
+        }
+    }
+}
